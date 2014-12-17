c0cb53b99b0bc8c414e8c2f68b1b4ee53d564dd1
eynar <eynar@labware.com.bo>
2014-12-16 23:11:00 -0400

    Completando pagos de ambientes

--- app/Controller/ConceptosController.php
+++ app/Controller/ConceptosController.php
@@ -37,5 +37,34 @@ class ConceptosController extends AppController {
         $this->Edificioconcepto->save($this->request->data['Edificioconcepto']);
         exit;
     }
+    //Modulo de servicios de ambientes
+    public function aservicios($idAmbiente = NULL,$idEdificio = NULL)
+    {
+        $this->layout = 'ajax';
+        $servicios = $this->Ambienteconcepto->findAllByambiente_id($idAmbiente);
+        $conceptos = $this->Concepto->find('list',array('fields' => 'Concepto.nombre','conditions' => array('Concepto.edificio_id' => $idEdificio)));
+        $this->set(compact('servicios','idEdificio','conceptos','idAmbiente'));
+    }
+    public function guarda_nuevo_servicio_a()
+    {
+        $this->Concepto->create();
+        $this->Concepto->save($this->request->data['Concepto']);
+        $idConcepto = $this->Concepto->getLastInsertID();
+        $this->Ambienteconcepto->create();
+        $this->request->data['Ambienteconcepto']['concepto_id'] = $idConcepto;
+        $this->Ambienteconcepto->save($this->request->data['Ambienteconcepto']);
+        exit;
+    }
+    public function quita_servicio_a($idAmbConcepto = null,$idAmbiente = NULL,$idEdificio = NULL)
+    {
+        $this->Ambienteconcepto->delete($idAmbConcepto);
+        $this->redirect(array('action' => 'aservicios',$idAmbiente,$idEdificio));
+    }
+    public function guarda_servicio_a()
+    {
+        $this->Ambienteconcepto->create();
+        $this->Ambienteconcepto->save($this->request->data['Ambienteconcepto']);
+        exit;
+    }
 }
 
--- app/View/Ambientes/ambiente.ctp
+++ app/View/Ambientes/ambiente.ctp
@@ -68,6 +68,7 @@
         <div class="col-xs-12 text-right">
             
             <button type="button" class="btn btn-sm btn-default" data-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-sm btn-info" onclick="cargarmodal('<?php echo $this->Html->url(array('controller' => 'Conceptos','action' => 'aservicios',$idAmbiente,$piso['Edificio']['id']))?>');">Servicios</button>
             <button type="button" class="btn btn-sm btn-warning" onclick="cargarmodal('<?php echo $this->Html->url(array('action' => 'inquilinos',$idAmbiente))?>');">Inquilinos</button>
             <button type="submit" class="btn btn-sm btn-primary">Guardar</button>
         </div>
